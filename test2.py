17a8ec31629cef5794edcde9aa907b142c73d8c0
vfcsz03 <fysccz03@gmail.com>
2017-03-28 23:53:00 +0000

    Add files via upload

--- test2.py
+++ test2.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import time
 
 freq = 32
-sampling_rate=1000
+sampling_rate=55
 
 t= np.linspace (0, 2, 2 *sampling_rate, endpoint=False)
 x1= np.sin(freq* 2* np.pi * t)
 x2= np.cos(5* 2* np.pi * t)
 x3= np.sin(25* 2* np.pi * t)
 
-x=x1
+x=x1+x2+x3
 
 fig = plt.figure(1)
 ax1 = fig.add_subplot(211)
